b7c75d94df53f8820a3b30df6bd2bcb71d8e39b8
Mohammed Elnogomy <144149545+melnegomy@users.noreply.github.com>
2025-08-23 21:31:28 +0300

    Add files via upload

---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace InventoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // add product (id name quantaty price)
+            // update product
+            // view product 
+            // exit 
+
+            while (true) { 
+            Console.WriteLine("Welcome To My InventoryManagementSystem");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("1. Add Product");
+            Console.WriteLine("2. Update Product");
+            Console.WriteLine("3. View Product");
+            Console.WriteLine("4. Exit");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+                {
+                 case 1:
+                        // add product 
+                        break;
+                 case 2:
+                        // update product
+                        break;
+                 case 3:
+                        // view product 
+                        break;
+                 case 4:
+                        // exit 
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+        private static void AddProduct()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
