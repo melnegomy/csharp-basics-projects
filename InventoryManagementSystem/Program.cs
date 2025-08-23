c64926b50e520526353d3884a80fe843f583ab7e
Mohammed Elnogomy <144149545+melnegomy@users.noreply.github.com>
2025-08-23 22:37:00 +0300

    Add files via upload

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -2,35 +2,42 @@
 {
     internal class Program
     {
+        const int numberOfProduct = 50;
+        static string[,] inventory = new string[ numberOfProduct , 3 ];
+        static int productcount = 0;
         static void Main(string[] args)
         {
             // add product (id name quantaty price)
             // update product
             // view product 
             // exit 
+            while (true)
+            {
+                Console.WriteLine("Welcome To My InventoryManagementSystem");
+                Console.WriteLine("=======================================");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Update Product");
+                Console.WriteLine("3. View Product");
+                Console.WriteLine("4. Exit");
 
-            while (true) { 
-            Console.WriteLine("Welcome To My InventoryManagementSystem");
-            Console.WriteLine("=======================================");
-            Console.WriteLine("1. Add Product");
-            Console.WriteLine("2. Update Product");
-            Console.WriteLine("3. View Product");
-            Console.WriteLine("4. Exit");
+                Console.WriteLine("Choose The Number");
+                int userInput = Convert.ToInt32(Console.ReadLine());
 
-            int userInput = Convert.ToInt32(Console.ReadLine());
-
-            switch (userInput)
+                switch (userInput)
                 {
-                 case 1:
-                        // add product 
+                    case 1:
+                        // add product
+                        AddProduct();
                         break;
-                 case 2:
+                    case 2:
                         // update product
+                        updateProduct();
                         break;
-                 case 3:
+                    case 3:
                         // view product 
+                        ViewProduct();
                         break;
-                 case 4:
+                    case 4:
                         // exit 
                         Environment.Exit(0);
                         break;
@@ -39,54 +46,84 @@
 
             }
 
+        }
+        private static void AddProduct()
+        {
+            Console.WriteLine("Enter Product Name : ");
+            string productName = Console.ReadLine();
+
+            Console.WriteLine("Enter Product Quantaty : ");
+            string productQuantaty = Console.ReadLine();
+
+            Console.WriteLine("Enter Product Price : ");
+            string productPrice = Console.ReadLine();
 
 
+            inventory[productcount, 0] = productName;
+            inventory[productcount, 1] = productQuantaty;
+            inventory[productcount, 2] = productPrice;
 
+            productcount++;
 
+            Console.WriteLine("Product Added Successfuly");
 
+        }
+        private static void updateProduct()
+        {
 
+            Console.WriteLine("Enter The Name Product To Update :");
+            string searchProduct = Console.ReadLine();
 
+            int productID = -1;
 
+            if (productcount > 0)
+            {
+                for (int i = 0; i < productcount; i++)
+                {
 
+                    if (inventory[i, 0] == searchProduct)
+                    {
+                        productID = i;
+                        break;
+                    }
 
+                }
+                if (productID != -1)
+                {
+                    Console.WriteLine("Enter The New Quantaty");
+                    string newQantaty = Console.ReadLine();
 
+                    inventory[productID, 1] = newQantaty;
 
-
-
-
-
-
-
-
+                    Console.WriteLine("Quantaty Updated Successfuly");
+                }
 
 
             }
-
-        private static void AddProduct()
+            else
+            {
+                Console.WriteLine("Inventory Is Empty");
+            }
+        }
+        private static void ViewProduct()
         {
+            if (productcount > 0)
+            {
+
+                Console.WriteLine("Product List :");
+                Console.WriteLine();
+                for (int i = 0; i < productcount; i++)
+                {
+
+                    Console.WriteLine($"Product Id :{i} , Product Name :{inventory[i, 0]} , Product Quantaty : {inventory[i, 1]} , Product Price :{inventory[i, 2]}");
+                }
+
+            }
+            else 
+            {
+                Console.WriteLine("Inventory Is Empty");
+            }
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
